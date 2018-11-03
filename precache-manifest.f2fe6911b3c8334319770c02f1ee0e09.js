38655c64962f56fa7bc0890a61b2e8796fc2650d
Felipe-BP <gvv.fel@gmail.com>
2018-11-02 22:47:23 -0300

    Updates

---
+++ precache-manifest.f2fe6911b3c8334319770c02f1ee0e09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "073838767c020bb1afe2",
+    "url": "/t2/static/js/runtime~main.07383876.js"
+  },
+  {
+    "revision": "fe43c7849547bcd7d710",
+    "url": "/t2/static/js/main.fe43c784.chunk.js"
+  },
+  {
+    "revision": "ebc5ddfdbebe2a3d0b23",
+    "url": "/t2/static/js/1.ebc5ddfd.chunk.js"
+  },
+  {
+    "revision": "fe43c7849547bcd7d710",
+    "url": "/t2/static/css/main.059898d1.chunk.css"
+  },
+  {
+    "revision": "b0aeb04f4ab070822c69eb2f744481a8",
+    "url": "/t2/index.html"
+  }
+];
